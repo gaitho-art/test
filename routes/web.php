827cb69fc950fe1c229db8306cedcb786b593743
gaitho-art <deewahome@gmail.com>
2025-05-27 12:11:23 +0300

    dashboard intergration

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Dashboard\HomeController as DashboardHomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardHomeController::class, 'index'])->name('dashboard')->middleware(['auth']);
